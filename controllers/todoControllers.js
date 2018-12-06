935c632f9f61fba0739e8cb95ef0f243c7215d81
Ambati Satvik <ambatisatvik@cse.iitb.ac.in>
2018-12-06 16:29:29 +0530

    Database added (mongodb)

--- controllers/todoControllers.js
+++ controllers/todoControllers.js
@@ -1,25 +1,42 @@
 var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
 
-var data = [{item: 'breakfast'}, {item: 'walking'}, {item: 'coding'}];
+mongoose.connect('mongodb://tester:tester123@ds213259.mlab.com:13259/firstapp')
+
+//Create a schema - like a blueprint
+var todoSchema = new mongoose.Schema({
+  item: String
+});
+
+var Todo = mongoose.model('Todo',todoSchema);
+
+//var data = [{item: 'breakfast'}, {item: 'walking'}, {item: 'coding'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 module.exports = function(app){
 
 app.get('/todo',function(req,res){
-  res.render('todo', {todos:data});
+  //get data from mongodb
+  Todo.find({}, function(err, data){
+    if (err) throw err;
+    res.render('todo', {todos:data});
+  });
 });
 
 app.post('/todo',urlencodedParser, function(req,res){
 
-  data.push(req.body);
-  res.json({todos:data});
+  var newTodo = Todo(req.body).save(function(err,data){
+    if (err) throw err;
+    res.json(data);
+  });
   //res.render('todo', {todos:data});
 });
 
 app.delete('/todo/:item',function(req,res){
-  data = data.filter(function(todo){
-    return todo.item.replace(/ /g, '-') !== req.params.item;
+
+  Todo.find({item: req.params.item.replace(/\-/g, " ")}).remove(function(err,data){
+    if (err) throw err;
+    res.json(data);
   });
-  res.json({todos:data});
 });
 
 }

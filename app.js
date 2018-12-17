63f38c0f844c127577696603baef38fe4941b723
Ambati Satvik <ambatisatvik@cse.iitb.ac.in>
2018-12-17 17:09:34 +0530

    newman removed

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var todoController = require('./controllers/todoControllers');
 var app = express();
-var newman = require('newman'); // require newman in your project
+//var newman = require('newman'); // require newman in your project
 
 //set up template engine
 app.set('view engine','ejs');
@@ -20,10 +20,10 @@ console.log('You are listening to port 3000');
 
 
 // call newman.run to pass `options` object and wait for callback
-newman.run({
-    collection: require('./test.postman_collection.json'),
-    reporters: 'cli'
-}, function (err) {
-	if (err) { throw err; }
-    console.log('collection run complete!');
-});
+// newman.run({
+//     collection: require('./test.postman_collection.json'),
+//     reporters: 'cli'
+// }, function (err) {
+// 	if (err) { throw err; }
+//     console.log('collection run complete!');
+// });

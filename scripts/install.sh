1497c05f2b4cc14a51279d099164bb1edb02fdfd
Ambati Satvik <ambatisatvik@cse.iitb.ac.in>
2018-12-27 00:16:22 +0530

    Hoping for the best

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,5 +5,5 @@ sudo curl -sL https://rpm.nodesource.com/setup_9.x | sudo -E bash -
 sudo yum install nodejs
 
 yum load-transaction /tmp/yum_save_tx.2018-12-26.18-41.fvQbtl.yumtx
-
+sudo apt-get install npm
 sudo npm install forever -g

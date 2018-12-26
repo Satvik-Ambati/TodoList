98623ea30ac54281649a41c510ea12a792052368
Ambati Satvik <ambatisatvik@cse.iitb.ac.in>
2018-12-27 00:05:36 +0530

    Hoping for the best

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 yum install -y gcc-c++ make
-curl -sL https://rpm.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://rpm.nodesource.com/setup_9.x | sudo -E bash -
 yum install nodejs
+
+npm install forever -g

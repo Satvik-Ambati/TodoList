88929fc07ad0186e87b23f709ea7814334cbb134
Ambati Satvik <ambatisatvik@cse.iitb.ac.in>
2018-12-27 01:34:28 +0530

    npm install

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,24 +3,40 @@
 #sudo yum install -y gcc-c++ make
 # sudo curl -sL https://rpm.nodesource.com/setup_9.x | sudo -E bash -
 # sudo yum install nodejs
-
 #sudo yum install nodejs npm --enablerepo=epel
-
 #yum load-transaction /tmp/yum_save_tx.2018-12-26.18-41.fvQbtl.yumtx
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm --version
 
-nvm install node
-sudo yum install gcc-c++ make
+# yum -y update
+# curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+# . ~/.nvm/nvm.sh
+# nvm --version
+
+# nvm install node
+# sudo yum install gcc-c++ make
+
+# node --version
+# npm --version
+
+# $PATH
+# whereis npm 
+# whereis node
+#cp /root/.nvm/versions/node/v11.6.0/bin/npm /usr/bin/env
+#sudo apt-get install npm
+
+
+set -e
+yum -y update
+# install general libraries like Java or ImageMagick
+yum -y install default-jre ImageMagick
+
+sudo curl -sL https://rpm.nodesource.com/setup_10.x | sudo -E bash -
+yum -y install nodejs
 
 node --version
 npm --version
 
-$PATH
-whereis npm 
 whereis node
-#cp /root/.nvm/versions/node/v11.6.0/bin/npm /usr/bin/env
-#sudo apt-get install npm
-sudo npm install forever -g
+whereis npm
+
+sudo npm install -g forever

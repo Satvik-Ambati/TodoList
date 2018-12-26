ce25542f3bf202f633b51cd4c2ba49c4dd27230c
Ambati Satvik <ambatisatvik@cse.iitb.ac.in>
2018-12-27 00:48:28 +0530

    npm install error

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,9 +4,18 @@
 # sudo curl -sL https://rpm.nodesource.com/setup_9.x | sudo -E bash -
 # sudo yum install nodejs
 
-sudo yum install nodejs npm --enablerepo=epel
+#sudo yum install nodejs npm --enablerepo=epel
 
 #yum load-transaction /tmp/yum_save_tx.2018-12-26.18-41.fvQbtl.yumtx
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+nvm --version
+
+nvm install node
+sudo yum install gcc-c++ make
+
+node --version
+npm --version
 
 $PATH
 whereis npm 

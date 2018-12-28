0e6e32e543d39879dbdbda6a0be65118cfe45b9c
Ambati Satvik <ambatisatvik@cse.iitb.ac.in>
2018-12-28 12:34:04 +0530

    check

--- test/test-pages.js
+++
@@ -1,9 +0,0 @@
-var expect  = require('chai').expect;
-var request = require('request');
-
-it('About page content', function(done) {
-    request('http://localhost:3000/todo' , function(error, response, body) {
-        expect(response.statusCode).to.equal(200);
-        done();
-    });
-});
